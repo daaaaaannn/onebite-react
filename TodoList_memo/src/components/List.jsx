442e9f4e89a1b06d08b8420010e04e802ca2f616
DaEun Kim <rlaekdms901@naver.com>
2025-03-19 19:16:11 +0900

    TodoList 최적화

---
+++ TodoList_memo/src/components/List.jsx
@@ -0,0 +1,70 @@
+import './List.css';
+import TodoItem from './TodoItem';
+import { useState, useMemo } from 'react';
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log('호출');
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // 의존성 배열, deps
+  // 콜백함수가 반환하는 값을 그대로 반환해줌
+  // 콜백 함수를 deps를 기준으로 메모이제이션 함
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  return (
+    <div className="List">
+      <h4>Todo List🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
